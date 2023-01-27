a627703d22f4daa005a59990c6025e0429524631
AID5 <aiden.l.fleming@gmail.com>
2023-01-27 21:09:31 +1100

    candidate-js-live-test MVP
    - Support for adding square to the canvas
    - Support for changing the color of a selected square by picking a color from a dropdown list.
    - Support for evenly spacing squares on the canvas
    - Created Button Component

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex space-x-2 justify-center">
+    <button @click="onClick" type="button" v-bind:class="getClass()">
+      <slot>Button</slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    onClick: {
+      type: Function,
+      required: true,
+    },
+    disable: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  methods: {
+    getClass() {
+      const base =
+        "mb-2 w-full px-6 py-2.5 text-white font-medium text-xs leading-normal uppercase rounded shadow-md transition duration-150 ease-in-out";
+      const classNames = this.disable
+        ? "bg-gray-400  pointer-events-none"
+        : "bg-gray-500 hover:bg-gray-600 hover:shadow-lg active:shadow-lg";
+      return `${base} ${classNames}`;
+    },
+  },
+};
+</script>
--- src/components/Maker.vue
+++ src/components/Maker.vue
@@ -1,56 +1,174 @@
 <template>
   <div class="hello">
-    <h1 class="text-center">Maker</h1>
-    <div class="">
-      <canvas class="" ref="can" width="800" height="800"></canvas>
-      <div id=controls>
-        <button id="addNewSquare" @click="addNewSquare">Add New Square</button>
-        <button id="evenlySpaceVertically" @click="evenlySpaceVertically">Evenly Space Vertically</button>
-        <button id="getCurrentObjects" @click="getCurrentCanvasObjects">Console Log Current Objects</button>
-        <button id="reRenderCanvas" @click="reRenderCanvas">Re-Render Canvas</button>
+    <h1 class="text-center font-bold text-2xl text-white bg-gray-500">Maker</h1>
+    <div class="flex">
+      <div>
+        <canvas
+          class="border-2 m-2"
+          ref="can"
+          width="800"
+          height="800"
+        ></canvas>
+      </div>
+      <div
+        id="controls"
+        class="bg-gray-200 w-full ml-2 grid items-center justify-center m-2"
+      >
+        <v-button id="addNewSquare" :onClick="addNewSquare"
+          >Add New Square</v-button
+        >
+        <v-button id="evenlySpaceVertically" :onClick="evenlySpaceVertically"
+          >Evenly Space Vertically</v-button
+        >
+        <v-button id="getCurrentObjects" :onClick="getCurrentCanvasObjects"
+          >Console Log Current Objects</v-button
+        >
+        <v-button id="reRenderCanvas" :onClick="reRenderCanvas"
+          >Re-Render Canvas</v-button
+        >
+        <div>
+          <label for="colour">Change Colour</label>
+          <select
+            id="colour"
+            @change="updateColour($event)"
+            v-model="currentObjectColour"
+          >
+            <option disabled value="">Please Select</option>
+            <option
+              v-for="colour in colours"
+              :value="colour"
+              v-bind:key="colour"
+            >
+              {{ colour }}
+            </option>
+          </select>
+        </div>
+        <div class="flex-row">
+          <v-button id="reRenderCanvas" class="w-1/2" :onClick="reRenderCanvas"
+            >Undo</v-button
+          >
+          <v-button
+            id="reRenderCanvas"
+            class="w-1/2"
+            :onClick="reRenderCanvas"
+            v-bind:disable="redoButtonDisabled"
+            >Redo</v-button
+          >
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { fabric } from 'fabric';
+import { fabric } from "fabric";
+import Button from "./Button";
+
+const rect = {
+  fill: "red",
+  width: 100,
+  height: 100,
+  left: 0,
+  top: 0,
+};
 
 export default {
-  name: 'Maker',
+  name: "Maker",
   data: () => ({
     canvas: null,
+    key: "",
+    redoButtonDisabled: true,
+    currentObjectColour: "",
+    colours: ["Red", "Green", "Blue", "Yellow", "Pink", "Purple"],
   }),
   mounted() {
     const ref = this.$refs.can;
     this.canvas = new fabric.Canvas(ref);
-    const rect = new fabric.Rect({
-      fill: 'red',
-      width: 100,
-      height: 100,
-      left:0,
-      top:0
+    this.addNewSquare();
+
+    this.canvas.on({
+      "object:selected": this.addNewSquare(),
     });
-    this.canvas.add(rect);
   },
   methods: {
     addNewSquare() {
-      //To be implemented
+      this.canvas.add(new fabric.Rect(rect));
     },
-    evenlySpaceVertically(){
-      //To be implemented
+    evenlySpaceVertically() {
+      let objects = this.canvas.getObjects();
+      let leftOffset = 0;
+      let topOffset = 0;
+      let colMaxWidth = 0;
+      let padding = 10;
+      objects.forEach((obj, index) => {
+        const objWidth = obj.getScaledWidth();
+        const objHeight = obj.getScaledHeight();
+        obj.set("left", leftOffset);
+        obj.set("top", topOffset);
+        obj.setCoords();
+        topOffset += objHeight + padding;
+
+        if (objWidth > colMaxWidth) colMaxWidth = objWidth;
+
+        // Check if the next object will fit under the current object
+        if (objects[index + 1] != null) {
+          if (
+            topOffset + objects[index + 1].getScaledHeight() >
+            this.canvas.height
+          ) {
+            // If object can't fit then move to next row
+            topOffset = 0;
+            leftOffset += colMaxWidth + padding;
+            colMaxWidth = 0;
+          }
+        }
+      });
+      this.canvas.renderAll();
     },
     reRenderCanvas() {
       this.canvas.renderAll();
+      this.redoButtonDisabled = true;
+      console.log(this.redoButtonDisabled);
     },
-    getCurrentCanvasObjects(){
+    getCurrentCanvasObjects() {
       console.log(this.canvas.getObjects());
       return this.canvas.getObjects();
     },
-  }
-}
+    updateColour(event) {
+      if (!this.canvas.getActiveObject()) {
+        console.log("No active object");
+        return;
+      }
+
+      this.canvas.getActiveObject().set("fill", event.target.value);
+      this.canvas.renderAll();
+    },
+    // handleObjectSelected(event) {
+    //   console.log(event.target);
+    // },
+  },
+  components: {
+    "v-button": Button,
+  },
+};
 </script>
 
 <style scoped>
-
+#controls {
+  height: 800px;
+}
+label {
+  --tw-bg-opacity: 1;
+  background-color: rgba(107, 114, 128, var(--tw-bg-opacity));
+  cursor: pointer;
+  color: white;
+  height: 45px;
+}
+label,
+select {
+  padding: 0.876rem;
+}
+select {
+  width: 300px;
+}
 </style>
